43c69cc3ca5f9d9de9172738068f02c57fa03b48
redlemon8 <hanquezbenjamin@gmx.com>
2023-06-11 02:24:06 +0200

    popup acount.js check user

--- popup-acount.js
+++ popup-acount.js
@@ -5,3 +5,24 @@ function openForm() {
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
+
+class user {
+    constructor(name, firstName, email, password){
+        this.name = name,
+        this.firstName = firstName,
+        this.email = email,
+        this.password = password
+    }
+
+    validInscription() {
+        if (!this.name || this.firstName || !this.email || !this.password) {
+            console.log("Veuillez remplir tous les champs.");
+        }
+    
+      console.log("Inscription validée !");
+      return true;
+    }
+}
+
+const newUser = new user ();
+newUser.validInscription();
